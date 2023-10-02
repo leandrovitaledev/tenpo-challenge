a5c52bb8872ee0cd9362e24d18cb4c9c79c586d3
leandrovitaledev <leandrovitaledev@gmail.com>
2023-10-02 18:32:11 -0300

    initial commit

---
+++ src/main/java/com/example/tenpochallenge/TenpoChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tenpochallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenpoChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenpoChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tenpochallenge/TenpoChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tenpochallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenpoChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
